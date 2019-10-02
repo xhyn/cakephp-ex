33121d8427a63cdb1a70e1083a9868416f74627a
xhyn <31637619+xhyn@users.noreply.github.com>
2019-10-02 15:05:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,104 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-image: url("bg.jpg");
+    background-repeat: no-repeat;
+    background-position: right top;
+    margin-right: 200px;
+    background-attachment: fixed;
+}
 
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+h1 {
+    color: white;
+    text-align: center;
+}
+
+p1 {
+	color: white;
+    font-family: verdana;
+    font-size: 20px;
+}
+p2 {
+	color: yellow;
+    font-family: Arial;
+    font-size: 20px;
+}
+p3 {
+	color: red;
+    font-family: Times New Roman;
+    font-size: 20px;
+}
+p4 {
+	color: purple;
+    font-family: Lucida Console;
+    font-size: 20px;
+}
+p5 {
+	color: orange;
+    font-family: Georgia;
+    font-size: 20px;
+}
+
+img {
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+    width: 600px;
+}
+
+img:hover {
+    box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+
+</style>
+</head>
+<body>
+
+<h1>Halaman WEB</h1>
+<p1>Iki paragraf <br>duduk paranormal.<br></p1>
+
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+<p2> Asolole<br></p2>
+<p3> Asolole<br></p3>
+<p4> Asolole<br></p4>
+<p5> Asolole<br></p5>
+
+<a target="_blank" href="xxx.jpg">
+  <img src="xxx.jpg" alt="Zanafar" style="width:600px">
+</a>
+<br>
+<?php
+date_default_timezone_set('Asia/Jakarta');
+echo date('l jS \of F Y h:i:s A');
+?>
+</body>
+</html>
